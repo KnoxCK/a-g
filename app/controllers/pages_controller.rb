c1fb6453ad62317c25a5d541a4ab9fdfdec701e9
Clarissa Knox <knox.ck@gmail.com>
2017-10-12 13:20:36 +0100

    landing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :about_us, :contact]
 
   def home
   end
+
+  def about_us
+  end
+
+  def contact
+  end
+
 end
